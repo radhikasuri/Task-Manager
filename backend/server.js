aa8dd85909fd94b4126db6edd1ee8bc024c6ec95
Radhika Suri <radhika.suri@ericsson.com>
2026-05-17 22:15:09 +0530

    fix cors link

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "task-manager-rouge-theta-35.vercel.app"
+      "https://task-manager-rouge-theta-35.vercel.app"
     ],
     credentials: true,
   })
